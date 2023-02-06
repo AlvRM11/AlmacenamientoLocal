dd169fdeb2556836e2d6b609c4397280d5cd1253
Alvaro <usr2725@salesianos-lacuensta.net>
2023-02-06 13:05:06 +0000

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function App() {
+const App = () => {
+  const [value, setValue] = useState('');
+  const [storedValue, setStoredValue] = useState('');
+
+  const storeData = async () => {
+    try {
+      await AsyncStorage.setItem('@storage_Key', value);
+      setStoredValue(value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const retrieveData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@storage_Key');
+
+      if (value !== null) {
+        setStoredValue(value);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.childContainer}>
+        <TextInput style={styles.textInput} onChangeText={text => setValue(text)} value={value} />
+        <Button title='Store Data' onPress={storeData} />
+        <Button title='Retrieve Data' onPress={retrieveData} />
+        <Text>Stored Value: {storedValue}</Text>
+      </View>
     </View>
   );
 }
@@ -15,6 +44,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  childContainer: {
+    width: 200,
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'space-evenly'
+  },
+  textInput: {
+    width: 120,
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1
+  }
 });
+
+export default App;
